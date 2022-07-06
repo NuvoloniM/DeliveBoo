d7124a42e903b64a365fb71daecfec5c5f44e332
NuvoloniM <nuvoloni.m@gmail.com>
2022-07-06 10:04:48 +0200

    .

--- public/js/app.js
+++ public/js/app.js
@@ -37333,15 +37333,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/backStyle.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/backStyle.scss ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/resources/sass/backStyle.scss'\n    at /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (node:internal/process/task_queues:78:11)");
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*********************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/backStyle.scss ***!
+  \*********************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Vincenzo\Desktop\6 - Mega Esercizio\DeliveBoo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Vincenzo\Desktop\6 - Mega Esercizio\DeliveBoo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/matteonuvoloni/Desktop/Boolean/DeliveBoo/resources/sass/backStyle.scss */"./resources/sass/backStyle.scss");
 
 
 /***/ })
