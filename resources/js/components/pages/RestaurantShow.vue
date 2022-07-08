5a61198c80ce9284ed98c9aca9557e8cd3f3a471
NuvoloniM <nuvoloni.m@gmail.com>
2022-07-08 15:00:57 +0200

    form pagamento

--- resources/js/components/pages/RestaurantShow.vue
+++ resources/js/components/pages/RestaurantShow.vue
@@ -43,6 +43,48 @@
                 </div>
             </div>
         </div>
+
+    <!-- fomr pagamento da cancelare  -->
+
+        <div class="alert alert-danger" v-if="error">
+            {{ error }}
+        </div>
+
+        <form>
+            <div class="form-group">
+                <label for="amount">Amount</label>
+                <div class="input-group">
+                    <div class="input-group-prepend"><span class="input-group-text">$</span></div>
+                    <input type="number" id="amount" class="form-control" placeholder="Enter Amount">
+                </div>
+            </div>
+                <hr />
+            <div class="form-group">
+                <label>Credit Card Number</label>
+                <div id="creditCardNumber" class="form-control"></div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-6">
+                        <label>Expire Date</label>
+                        <div id="expireDate" class="form-control"></div>
+                    </div>
+                    <div class="col-6">
+                        <label>CVV</label>
+                        <div id="cvv" class="form-control"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-light">
+                <div class="card-header">Payment Information</div>
+                <div class="card-body">
+                    <div class="alert alert-success" v-if="nonce">
+                        Successfully generated nonce.
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-primary btn-block" @click.prevent="payWithCreditCard">Pay with Credit Card</button>
+        </form>
     </div>
 
 
@@ -51,15 +93,23 @@
 <script>
 
 import axios from 'axios';
+// import PaymentForm from './PaymentForm.vue';
+import braintree from 'braintree-web';
 
 export default {
     name: 'RestaurantShow',
+    components: {
+        // PaymentForm,
+    },
     data(){
         return{
             count: 0,
             menu: [],
             carrello:[],
             totalPrice: 0,
+            hostedFieldInstance: false,
+            nonce: '',
+            error:'',
         }
     },
     methods: {
@@ -135,11 +185,62 @@ export default {
         },
         deleteCart(){
             this.carrello = [];
+        },
+        payWithCreditCard() {
+        if(this.hostedFieldInstance)
+        {
+            this.error = "";
+            this.nonce = "";
+            
+            this.hostedFieldInstance.tokenize().then(payload => {
+                console.log(payload);
+                this.nonce = payload.nonce;
+            })
+            .catch(err => {
+                console.error(err);
+                this.error = err.message;
+            })
         }
+   }
     },
     mounted(){
         this.getDish();
-    }
+       braintree.client.create({
+           authorization: "sandbox_hch5bftk_txgvrjhb35ytsksf"
+       })
+       .then(clientInstance => {
+           let options = {
+               client: clientInstance,
+               styles: {
+                   input: {
+                       'font-size': '14px',
+                       'font-family': 'Open Sans'
+                   }
+               },
+               fields: {
+                   number: {
+                       selector: '#creditCardNumber',
+                       placeholder: 'Enter Credit Card'
+                   },
+                   cvv: {
+                       selector: '#cvv',
+                       placeholder: 'Enter CVV'
+                   },
+                   expirationDate: {
+                       selector: '#expireDate',
+                       placeholder: '00 / 0000'
+                   }
+               }
+           }
+           return braintree.hostedFields.create(options)
+       })
+       .then(hostedFieldInstance => {
+           // @TODO - Use hostedFieldInstance to send data to Braintree
+           this.hostedFieldInstance = hostedFieldInstance;
+       })
+       .catch(err => {
+       });
+   }
 }
 </script>
 
