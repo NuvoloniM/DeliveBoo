188544f24e85c2066ecb0d3e10cf54f0615f037c
AlbertoBiga <alberto.biga10@gmail.com>
2022-07-14 16:09:12 +0200

    .

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-generale d-flex">
         <div class="personal dashboard">
-            <ul class="personal-ul">
+            <ul class="personal-ul mt-5">
                 <li>
                     <a class="personal-a white" href="{{route('admin.restaurants.index')}}">Home</a>
                 </li>
@@ -45,7 +45,7 @@
                     </h1>
                 </div>
                 <div class="container">
-                    <div class="row d-flex jusify-content-center">
+                    <div class="row d-flex justify-content-center">
                         @forelse ($restaurants as $restaurant)
                         <div class="col-6 text-center rounded-lg mt-3 p-3">
                             <a href="{{route('admin.restaurants.show', $restaurant->id)}}">
