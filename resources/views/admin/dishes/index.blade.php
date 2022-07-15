54627da5ef5835c108a183746c0ece8c29e2128b
AlbertoBiga <alberto.biga10@gmail.com>
2022-07-15 15:44:27 +0200

    show ristorante responsive

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -2,133 +2,114 @@
 
 @section('content')
 <div class="over">
-
-
-        <div class="container my-5">
-            <div class="row">
-                <div class="col-md-4 col-sm-6">
-                    <div class="personal dashboard sticky-top">
-                        <ul class="personal-ul mt-5">
-                            <li>
-                                <img
-                                src="{{ asset('img/magnapp-logo.png') }}"
-                                width="100"
-                                alt="moto"
-                                />
-                            </li>
-                            <li>
-                                <a class="personal-a white" href="/">Home</a>
-                            </li>
-                            <li>
-                                <a class="personal-a white" href="{{route('admin.restaurants.index')}}">Le tue attività</a>
-                            </li>
-                            <li>
-                                <a class="personal-a white"
-                                    href="{{route('admin.restaurants.dishes.create',['restaurant'=> $data])}}">Aggiungi
-                                    piatto</a>
-                            </li>
-                            <li>
-                                <a class="personal-a white" href="#">I tuoi ordini</a>
-                            </li>
-                            <li>
-                                <a class="personal-a white" href="#">Il tuo profilo</a>
-                            </li>
-                            <li>
-                                <a class="personal-a white" href="{{ route('logout') }}" onclick="event.preventDefault();
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="personal dashboard sticky-top">
+                    <ul class="personal-ul mt-5">
+                        <li>
+                            <img src="{{ asset('img/magnapp-logo.png') }}" width="100" alt="moto" />
+                        </li>
+                        <li>
+                            <a class="personal-a white" href="/">Home</a>
+                        </li>
+                        <li>
+                            <a class="personal-a white" href="{{route('admin.restaurants.index')}}">Le tue attività</a>
+                        </li>
+                        <li>
+                            <a class="personal-a white"
+                                href="{{route('admin.restaurants.dishes.create',['restaurant'=> $data])}}">Aggiungi
+                                piatto</a>
+                        </li>
+                        <li>
+                            <a class="personal-a white" href="#">I tuoi ordini</a>
+                        </li>
+                        <li>
+                            <a class="personal-a white" href="#">Il tuo profilo</a>
+                        </li>
+                        <li>
+                            <a class="personal-a white" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                             document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
-                                </a>
+                            </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
                 </div>
-            
-                <div class="col-8">
-                    @if ( session('message') )
-                    <div class="alert alert-info">
-                        {{ session('message') }}
-                    </div>
-                    @endif
-                    <h1 class="text-center blue">I tuoi piatti:</h1>
-                    <div class="row">
-                        @forelse ($dishes as $dish)
-                        <div class="col-xl-6 col-lg-9 mt-3 rounded-lg mt-2">
-                            <div class="d-flex flex-column justify-content-end mb-5 ">
-                                <div>
-                                    <div class="img rounded-lg d-flex justify-content-between align-items-end pb-3 {{($dish->view)? '' : 'cover'}}"
-                                        style="background-image: url({{asset("storage/$dish->immage")}})">
-                                        <div>
-                                            <a href="{{route('admin.restaurants.dishes.show',['restaurant'=> $data, $dish->id])}}"
-                                                class="btn btn-primary mb-3 ml-3">
-                                                View
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a href="{{route('admin.restaurants.dishes.edit',['restaurant'=> $data, $dish->id])}}"
-                                                class="btn btn-primary mb-3">
-                                                Edit
-                                            </a>
-                                        </div>
-                                        <div>
-                                            <a>
-                                                <form
+            </div>
+
+            <div class="col-8">
+                @if ( session('message') )
+                <div class="alert alert-info">
+                    {{ session('message') }}
+                </div>
+                @endif
+                <h1 class="text-center blue">I tuoi piatti:</h1>
+                <div class="row">
+                    @forelse ($dishes as $dish)
+                    <div class="col-xl-6 col-lg-9 mt-3 rounded-lg mt-2">
+                        <div class="d-flex flex-column justify-content-end mb-5 ">
+                            <div>
+                                <div class="img rounded-lg d-flex justify-content-between align-items-end pb-3 {{($dish->view)? '' : 'cover'}}"
+                                    style="background-image: url({{asset("storage/$dish->immage")}})">
+                                    <div>
+                                        <a href="{{route('admin.restaurants.dishes.show',['restaurant'=> $data, $dish->id])}}"
+                                            class="btn btn-primary mb-3 ml-3">
+                                            View
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <a href="{{route('admin.restaurants.dishes.edit',['restaurant'=> $data, $dish->id])}}"
+                                            class="btn btn-primary mb-3">
+                                            Edit
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <a>
+                                            <form
                                                 action="{{route('admin.restaurants.dishes.destroy', ['restaurant'=> $data, $dish->id])}}"
                                                 method="POST" class="delete-form">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-danger mr-3">
-                                                        Delete
-                                                    </button>
-                                                </form>                                        
-                                            </a>
-                                        </div>
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger mr-3">
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </a>
                                     </div>
                                 </div>
-                                <div class="name mt-3">
-                                    <h4 class="white">
-                                        {{ $dish->nome_prodotto }}
-                                    </h4>
-                                </div>
-                                <div class="mt-3 white">
-                                    <span>
-                                        Prezzo: {{ $dish->prezzo }}&euro;
-                                    </span>
-                                </div>
-                                {{-- <div class="mt-3 white">
-                                                        <span>
-                                                            {{ $dish->vegetariano }}
-                            </span>
-                        </div>
-                        <div class="mt-3 white">
-                            <span>
-                                {{ $dish->glutine }}
-                            </span>
-                        </div> --}}
-                        <div class="mt-3 white">
-                            <span>
-                                {{ $dish->description }}
-                            </span>
-                        </div>
+                            </div>
+                            <div class="name mt-3">
+                                <h4 class="white">
+                                    {{ $dish->nome_prodotto }}
+                                </h4>
+                            </div>
+                            <div class="mt-3 white">
+                                <span>
+                                    Prezzo: {{ $dish->prezzo }}&euro;
+                                </span>
+                            </div>
 
+                            <div class="mt-3 white">
+                                <span>
+                                    {{ $dish->description }}
+                                </span>
+                            </div>
+
+                        </div>
                     </div>
+                    @empty
+                    <div>
+                        non ci sono piatti
+                    </div>
+                    @endforelse
                 </div>
-                @empty
-                <div>
-                    non ci sono piatti
-                </div>
-                @endforelse
             </div>
         </div>
     </div>
-</div>
-
-
-
-
 </div>
 @endsection
 <style lang="scss" scoped>
@@ -141,9 +122,11 @@
     .blue {
         color: rgba(11, 99, 184, 1);
     }
-    .personal {
+
+    /* .personal {
         width: 220px;
-    }
+    } */
+
     .over {
         height: 100vh;
         overflow-y: auto;
@@ -174,7 +157,8 @@
         color: white;
     }
 
-    .cover{
+    .cover {
         opacity: 0.5;
     }
+
 </style>
--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -3,146 +3,82 @@
 @section('content')
 <div class="view">
     <div class="container-generale d-flex">
-        <div class="personal dashboard">
-            <ul class="personal-ul">
-                <li>
-                    <a class="personal-a white" href="{{route('admin.restaurants.index')}}">Home</a>
-                </li>
-                <li>
-                    <a class="personal-a white" href="{{route('admin.restaurants.create')}}">Aggiungi attività</a>
-                </li>
-                <li>
-                    <a class="personal-a white" a href="{{route('admin.restaurants.orders.index', $restaurant->id)}}">I tuoi ordini</a>
-                </li>
-                <li>
-                    <a class="personal-a white" href="#">Il tuo profilo</a>
-                </li>
-                <li>
-                    <a class="personal-a white" href="/">Ordina anche tu</a>
-                </li>
-                <li>
-                    <a class="personal-a white" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-    
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </li>
-            </ul>
-        </div>
-        <div class="activities">
-            @if ( session('message') )
-            <div class="alert alert-info">
-                {{ session('message') }}
+        <div class="row">
+            <div class=" col-md-4 col-sm-6 personal dashboard">
+                <ul class="personal-ul">
+                    <li>
+                        <a class="personal-a white" href="{{route('admin.restaurants.index')}}">Home</a>
+                    </li>
+                    <li>
+                        <a class="personal-a white" href="{{route('admin.restaurants.create')}}">Aggiungi attività</a>
+                    </li>
+                    <li>
+                        <a class="personal-a white" a href="{{route('admin.restaurants.orders.index', $restaurant->id)}}">I tuoi ordini</a>
+                    </li>
+                    <li>
+                        <a class="personal-a white" href="#">Il tuo profilo</a>
+                    </li>
+                    <li>
+                        <a class="personal-a white" href="/">Ordina anche tu</a>
+                    </li>
+                    <li>
+                        <a class="personal-a white" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+        
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                </ul>
             </div>
-            @endif
-            
-            <div class="over activities">
-                <div class="text-center blue title">
-                    <h1>
-                        Le tue attivita'
-                    </h1>
+            <div class="activities col-md-8 col-sm-6">
+                @if ( session('message') )
+                <div class="alert alert-info">
+                    {{ session('message') }}
                 </div>
-                <div class="container">
-                    <div class="row d-flex jusify-content-end">
-                        <div class="col-12 text-center rounded-lg mt-3 p-3">
-                            <a href="{{route('admin.restaurants.dishes.index', $restaurant->id)}}">
-                                <div class="d-flex flex-column justify-content-around">
-                                    <div class="img rounded-lg" style="background-image: url({{asset("storage/$restaurant->immagine")}})">
-
-                                    </div>
-                                    <div class="name mt-3">
-                                        <h3 class="white">
-                                            {{ $restaurant->nome_attivita }}
-                                        </h3>
-                                    </div>
-                                    <div class="mt-3 white">
-                                        <span>
-                                            {{ $restaurant->indirizzo }}
-                                        </span>
-                                    </div>
-                                    <div class="mt-3">
-                                        @include('includes.deleteRestaurant')
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
+                @endif
+                
+                <div class="over activities">
+                    <div class="text-center blue title">
+                        <h1>
+                            Le tue attivita'
+                        </h1>
                     </div>
-                </div>    
-            </div>
-            
+                    <div class="container">
+                        <div class="row d-flex jusify-content-end">
+                            <div class="col-12 text-center rounded-lg mt-3 p-3">
+                                <a href="{{route('admin.restaurants.dishes.index', $restaurant->id)}}">
+                                    <div class="d-flex flex-column justify-content-around">
+                                        <div class="img rounded-lg" style="background-image: url({{asset("storage/$restaurant->immagine")}})">
     
+                                        </div>
+                                        <div class="name mt-3">
+                                            <h3 class="white">
+                                                {{ $restaurant->nome_attivita }}
+                                            </h3>
+                                        </div>
+                                        <div class="mt-3 white">
+                                            <span>
+                                                {{ $restaurant->indirizzo }}
+                                            </span>
+                                        </div>
+                                        <div class="mt-3">
+                                            @include('includes.deleteRestaurant')
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
 
-    {{-- <div class=" container container-restaurant">
-        <div class="row justify-content-between">
-            <div class="col-6">
-                <div class="personal dashboard">
-                    <ul class="personal-ul">
-                        <li>
-                            <img
-                            src="{{ asset('img/magnapp-logo.png') }}"
-                            width="100"
-                            alt="moto"
-                            />
-                        </li>
-                        <li>
-                            <a class="personal-a white" href="/">Home</a>
-                        </li>
-                        <li>
-                            <a class="personal-a white" href="{{route('admin.restaurants.index')}}">Le tue attività</a>
-                        </li>
-                        <li>
-                            <a class="personal-a white" href="{{route('admin.restaurants.create')}}">Aggiungi attività</a>
-                        </li>
-                        <li>
-                            <a class="personal-a white" href="{{route('admin.order')}}">I tuoi ordini</a>
-                        </li>
-                        <li>
-                            <a class="personal-a white" href="#">Il tuo profilo</a>
-                        </li>
-                        <li>
-                            <a class="personal-a white" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-            
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-6 rounded-lg text-center">
-                <a href="{{route('admin.restaurants.dishes.index', $restaurant->id)}}">
-                    <div class="d-flex flex-column justify-content-around">
-                        <div class="img rounded-lg" style="background-image: url({{asset("storage/$restaurant->immagine")}})">
-                         {
-                        </div>
-                        <div class="name mt-3">
-                            <h3 class="white">
-                                {{ $restaurant->nome_attivita }}
-                            </h3>
-                        </div>
-                        <div class="mt-3 white">
-                            <span>
-                                {{ $restaurant->indirizzo }}
-                            </span>
-                        </div>
-                        <div class="mt-3">
-                            @include('includes.deleteRestaurant')
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div> --}}
     
 @endsection
 @section('scripts')
@@ -153,6 +89,9 @@
 
     .view{
         heigth: 100vh;
+    }
+    .row{
+        width: 100%;
     }
     
     .img{
@@ -175,13 +114,13 @@
     .personal-a:hover{
         color: white;
     }
-    .personal {
+    /* .personal {
         width: 220px;
-    }
-    .activities {
-        /* background-color: purple; */
+    } */
+    /* .activities {
+        
         width: calc(100% - 220px);
-    }
+    } */
     .blue {
         color: rgba(11, 99, 184, 1);
     }
